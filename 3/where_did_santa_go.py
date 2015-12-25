83ecdd0e7b92e76189ee010bbda473b615ef585a
Magnus Ivarsson <magnus@unitware.se>
2015-12-25 20:34:31 +0100

    day 3, final

--- 3/where_did_santa_go.py
+++ 3/where_did_santa_go.py
@@ -23,12 +23,26 @@ For example:
 > delivers presents to 2 houses: one at the starting location, and one to the east.
 ^>v< delivers presents to 4 houses in a square, including twice to the house at his starting/ending location.
 ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses.
+
+--- Part Two ---
+
+The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa, to deliver presents with him.
+
+Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), then take turns moving based on instructions from the elf, who is eggnoggedly reading from the same script as the previous year.
+
+This year, how many houses receive at least one present?
+
+For example:
+
+^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.
+^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
+^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
 '''
 
 EXAMPLES = [
-		('>', 2),
-		('^>v<', 4),
-		('^v^v^v^v^v', 2) 
+		('^v', 3),
+		('^>v<', 3),
+		('^v^v^v^v^v', 11) 
 	]
 
 def travel(direction, visited, lastpos):
@@ -53,20 +67,36 @@ def travel(direction, visited, lastpos):
 
 def main():
 	for data, num in EXAMPLES:
-		lastpos = '0,0'
-		visited = {lastpos: True}
+		santa_position = '0,0'
+		robo_position = '0,0'
+		visited = {santa_position: True}
 
+		santa_move = True
 		for direction in data:
-			lastpos = travel(direction, visited, lastpos)
+			if santa_move:
+				santa_position = travel(direction, visited, santa_position)
+			else:
+				robo_position = travel(direction, visited, robo_position)
+
+			santa_move = not santa_move
 
 		assert num == len(visited)
 		print('ok')
 
 	with open('input.txt') as fil:
-		lastpos = '0,0'
-		visited = {lastpos: True}
-		for direction in fil.read():
-			lastpos = travel(direction, visited, lastpos)
+		santa_position = '0,0'
+		robo_position = '0,0'
+		visited = {santa_position: True}
+		santa_move = True
+
+		directions = fil.read()
+		for direction in directions:
+			if santa_move:
+				santa_position = travel(direction, visited, santa_position)
+			else:
+				robo_position = travel(direction, visited, robo_position)
+
+			santa_move = not santa_move
 
 	print(len(visited))
 
